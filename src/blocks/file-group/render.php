ced51f5368ab40f2c8635ad7e3e41dcc32a6ca08
Leesa Ward <leesa@doubleedesign.com.au>
2025-12-13 10:02:35 +1100

    Refactor: Rename comet-plugin and comet-canvas to *-blocks

---
+++ src/blocks/file-group/render.php
@@ -0,0 +1,15 @@
+<?php
+use Doubleedesign\Comet\Core\FileGroup;
+
+$colorTheme = $block['colorTheme'] ?? 'primary';
+$files = array_map(function($file) {
+	return [
+		'url'         => $file['url'],
+		'title'       => $file['title'],
+		'description' => $file['description'],
+		'mimeType'    => $file['mime_type'],
+		// TODO: Add field for to have their own colour theme, so certain files can be highlighted by using a different colour
+	];
+}, get_field('files'));
+$component = new FileGroup(['colorTheme' => $colorTheme], $files);
+$component->render();
