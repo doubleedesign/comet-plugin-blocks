28b296195c2b8d9bcf4f1a47f9005fb0c6556124
Leesa Ward <leesa@doubleedesign.com.au>
2025-12-19 15:49:48 +1100

    Feat(WP Blocks): WIP on fields and controls for call-to-action, gallery, and accordion blocks

--- src/editor/CustomControlsWrapper/CustomControlsWrapper.dist.js
+++ src/editor/CustomControlsWrapper/CustomControlsWrapper.dist.js
@@ -3,16 +3,21 @@ import { ColorControls } from '../ColorControls/ColorControls.dist.js';
 
 /* global wp */
 const {
-  addFilter
+	addFilter
 } = wp.hooks;
 const {
-  InspectorControls
+	InspectorControls
 } = wp.blockEditor;
+
+/**
+ * Note: This file needs to be compiled (Rollup is configured for this)
+ * and loaded into the editor via the enqueue_block_editor_assets PHP hook
+ */
 wp.domReady(() => {
-  addFilter('editor.BlockEdit', 'comet-plugin-blocks/custom-controls', BlockEdit => props => {
-    return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", {
-      className: "comet-plugin-blocks-custom-controls"
-    }, /*#__PURE__*/React.createElement(InspectorControls, null, /*#__PURE__*/React.createElement(LayoutControls, props), /*#__PURE__*/React.createElement(ColorControls, props))), /*#__PURE__*/React.createElement(BlockEdit, props));
-  });
+	addFilter('editor.BlockEdit', 'comet-plugin-blocks/custom-controls', BlockEdit => props => {
+		return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement('div', {
+			className: 'comet-plugin-blocks-custom-controls'
+		}, /*#__PURE__*/React.createElement(InspectorControls, null, /*#__PURE__*/React.createElement(LayoutControls, props), /*#__PURE__*/React.createElement(ColorControls, props))), /*#__PURE__*/React.createElement(BlockEdit, props));
+	});
 });
 //# sourceMappingURL=CustomControlsWrapper.dist.js.map
