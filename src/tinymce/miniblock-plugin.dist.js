403571acf19810a7972a8c58b649042d7967df7c
Leesa Ward <leesa@doubleedesign.com.au>
2025-12-21 16:04:00 +1100

    Feat(WP Blocks): Add TinyMCE plugin for inserting and editing callouts

--- src/tinymce/miniblock-plugin.dist.js
+++ src/tinymce/miniblock-plugin.dist.js
@@ -2,7 +2,7 @@
 	'use strict';
 
 	class MiniblockPlugin {
-		constructor(editor) {
+		constructor() {
 			if (this.constructor === MiniblockPlugin) {
 				throw new Error('Cannot instantiate abstract class MiniblockPlugin directly');
 			}
@@ -30,7 +30,7 @@
 	class PullquotePlugin extends MiniblockPlugin {
 		/** @param {import('tinymce').Editor} editor */
 		constructor(editor) {
-			super(editor);
+			super();
 			const plugin = this;
 			editor.addButton('comet_miniblocks_pullquote', {
 				title: 'Pullquote',
@@ -110,11 +110,93 @@
 		}
 	}
 
+	class CalloutPlugin extends MiniblockPlugin {
+		/** @param {import('tinymce').Editor} editor */
+		constructor(editor) {
+			super();
+			const plugin = this;
+			editor.addButton('comet_miniblocks_callout', {
+				title: 'Callout',
+				icon: 'notice',
+				onclick: function () {
+					plugin.openModal(editor);
+				}
+			});
+			editor.on('click', function (e) {
+				// Handle clicks on existing callouts
+				/** @var {HTMLElement} */
+				const callout = editor.dom.getParent(e.target, 'div.callout');
+				if (callout) {
+					const data = {
+						content: decodeURIComponent(callout.getAttribute('data-content') || ''),
+						colorTheme: callout.getAttribute('data-color-theme') || ''
+					};
+					plugin.openModal(editor, data, callout);
+				}
+			});
+			editor.on('BeforeSetContent', function (e) {
+				// Ensure callouts stay non-editable when content is loaded
+				e.content = e.content.replace(
+					/<div class="callout"/g,
+					'<div class="callout" contenteditable="false"'
+				);
+			});
+		}
+
+		openModal(editor, existingData, existingNode) {
+			const data = existingData || { content: '', colorTheme: '' };
+
+			editor.windowManager.open({
+				title: existingNode ? 'Edit callout' : 'Insert callout',
+				body: [
+					{
+						type: 'textbox',
+						name: 'content',
+						label: 'Content',
+						value: data.content,
+						multiline: true,
+						minHeight: 100,
+					},
+					this.createColorThemeSelector(data.colorTheme)
+				],
+				onsubmit: function (e) {
+					const content = e.data.content.trim();
+					if (content === '') {
+						return;
+					}
+					const encodedContent = encodeURIComponent(content);
+					const colorTheme = e.data.colorTheme || '';
+					const calloutHtml = `
+					<div class="callout" 
+						data-content="${encodedContent}" 
+						data-color-theme="${colorTheme}" 
+						contenteditable="false">
+						<p>${content}</p>
+					</div>
+				`;
+					if (existingNode) {
+						existingNode.outerHTML = calloutHtml;
+					}
+					else {
+						editor.insertContent(calloutHtml);
+					}
+				}
+			});
+		}
+	}
+
 	/** @type {{ PluginManager: import('tinymce').AddOnManager }} */
 	const tinymce = window.tinymce;
 
+	/**
+	 * Note: compile this into one dist file using rollup with `npm run build` from the plugin root
+	 * Otherwise imports don't work because TinyMCE doesn't support ES modules for plugins.
+	 *
+	 * Plugin buttons also need to be specified in TinyMceConfig.php to be available in the editor.
+	 */
 	tinymce.PluginManager.add('comet_miniblocks', function (editor, url) {
 		new PullquotePlugin(editor);
+		new CalloutPlugin(editor);
 	});
 
 })();
