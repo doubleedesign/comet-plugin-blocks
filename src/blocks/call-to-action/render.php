d2812f3e28de6d25902a627389876ae634294f3e
Leesa Ward <leesa@doubleedesign.com.au>
2025-12-23 19:02:12 +1100

    Refactor(WP Blocks): Rename containerSize to size to match the PHP component

--- src/blocks/call-to-action/render.php
+++ src/blocks/call-to-action/render.php
@@ -1,2 +1,45 @@
 <?php
-echo 'TODO: Implement block rendering logic';
+/** @var $block array */
+use Doubleedesign\Comet\Core\{Button, ButtonGroup, CallToAction, ColorUtils, Config, Heading, Utils};
+use Doubleedesign\Comet\WordPress\PreprocessedHTML;
+
+$defaults = Config::getInstance()->get_component_defaults('call-to-action');
+$attributes = [
+    ...$defaults,
+    ...Utils::array_pick($block, ['size', 'colorTheme', 'backgroundColor']),
+    'innerBackground' => $block['colorTheme'] ?? 'primary',
+    'colorTheme'      => $block['colorTheme'] ? ColorUtils::get_readable_colour($block['colorTheme'], ['accent']) : 'primary',
+];
+
+$description = get_field('description');
+$buttons = get_field('buttons');
+$headingClasses = apply_filters('comet_blocks_cta_heading_classes', []);
+$buttonGroupAttrs = [
+    'colorTheme'  => $block['colorTheme'] ? ColorUtils::get_readable_colour($block['colorTheme'], ['accent']) : 'primary',
+    ...apply_filters('comet_blocks_cta_button_group_attributes', [])
+];
+
+$component = new CallToAction(
+    $attributes,
+    array(
+        new Heading(['classes' => $headingClasses], get_field('heading')),
+        ...(!empty($description) ? [new PreprocessedHTML([], $description)] : []),
+        new ButtonGroup(
+            $buttonGroupAttrs,
+            array_map(
+                function($button) use ($block) {
+                    return new Button(
+                        [
+                            'href'        => $button['link']['url'] ?? '#',
+                            'target'      => $button['link']['target'] ?? '',
+                            'isOutline'   => $button['style'] === 'isOutline',
+                        ],
+                        $button['link']['title']
+                    );
+                },
+                (is_array($buttons) ? $buttons : [])
+            )
+        )
+    ));
+
+$component->render();
