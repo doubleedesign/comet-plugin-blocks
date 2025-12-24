0c7c7b4386f035d97c9bd0337f9c170b2382016a
Leesa Ward <leesa@doubleedesign.com.au>
2025-12-24 12:33:35 +1100

    Fix(WP Blocks): Fix rendering of nested ACF blocks when loading in preview mode initially

--- src/blocks/shared-content/render.php
+++ src/blocks/shared-content/render.php
@@ -7,9 +7,23 @@ if (!isset($block['data']['shared_content_item'])) {
 }
 
 $shared_content_id = $block['data']['shared_content_item'];
-if ($shared_content_id) {
-    $post = get_post($shared_content_id);
-    if ($post) {
-        echo apply_filters('the_content', $post->post_content);
+if (!is_numeric($shared_content_id)) {
+    return;
+}
+
+$post = get_post($shared_content_id);
+if (!$post) {
+    return;
+}
+
+// Work around issue where nested ACF blocks do not render when loaded in preview mode on initial page load
+$is_editor = isset($is_preview) && $is_preview;
+if ($is_editor) {
+    $blocks = parse_blocks($post->post_content);
+    foreach ($blocks as $block_data) {
+        acf_render_block($block_data['attrs'], $post->post_content, true);
     }
+}
+else {
+    echo apply_filters('the_content', $post->post_content);
 }
