7c35e9640a1779cc4f8890e1ebdb609718d36f5a
Leesa Ward <leesa@doubleedesign.com.au>
2025-12-30 20:51:55 +1100

    Fix/feat(Accordion, WP Blocks): Tweak accordion markup and complete implementation for WP blocks

--- src/blocks/accordion/render.php
+++ src/blocks/accordion/render.php
@@ -1,3 +1,33 @@
 <?php
+
+use Doubleedesign\Comet\Core\{Accordion, AccordionPanel, Heading};
+use Doubleedesign\Comet\WordPress\PreprocessedHTML;
+
 /** @var $block array */
-echo 'TODO: Implement block rendering logic';
+$heading = get_field('heading');
+$intro_text = get_field('intro_copy');
+$colorTheme = $block['colorTheme'];
+
+$beforeComponents = [];
+if ($heading) {
+    array_push($beforeComponents, new Heading([], $heading));
+}
+if ($intro_text) {
+    array_push($beforeComponents, new PreprocessedHTML([], $intro_text));
+}
+
+$panelItems = get_field('panels');
+$panels = array_map(function($item) {
+    return new AccordionPanel(
+        ['title' => $item['heading']],
+        [new PreprocessedHTML([], $item['panel_content'])]
+    );
+}, $panelItems ?? []);
+
+$component = new Accordion(
+    ['colorTheme' => $colorTheme, 'size' => $block['size'] ?? 'contained'],
+    $panels,
+    $beforeComponents
+);
+
+$component->render();
