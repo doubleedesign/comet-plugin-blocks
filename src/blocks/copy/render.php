b4b7b71a4359ba8945730d6af88b5abe77defad5
Leesa Ward <leesa@doubleedesign.com.au>
2025-12-31 12:37:57 +1100

    Feat(WP Blocks): Add option to split Copy block into two columns

--- src/blocks/copy/render.php
+++ src/blocks/copy/render.php
@@ -1,15 +1,43 @@
 <?php
 /** @var $block array */
-use Doubleedesign\Comet\Core\Copy;
+
+use Doubleedesign\Comet\Core\{Column, Columns, Copy};
 use Doubleedesign\Comet\WordPress\PreprocessedHTML;
 
 $is_editor = isset($is_preview) && $is_preview;
 
-$component = new Copy([
+$has_second_column = get_field('field__copy__content__split') && !empty(get_field('field__copy__content_2'));
+$wrapperAttributes = [
     'size'       => $block['size'] ?? $block['attributes']['size']['default'],
-    'colorTheme' => $block['colorTheme'] ?? $block['attributes']['colorTheme']['default'],
-],
-    [new PreprocessedHTML([], get_field('copy'))]
-);
+];
+
+if ($has_second_column) {
+    $copyAttributes = [
+        'colorTheme' => $block['colorTheme'] ?? $block['attributes']['colorTheme']['default'],
+        'isNested'   => true,
+    ];
+
+    $first_column = new Copy($copyAttributes, [new PreprocessedHTML([], get_field('copy'))]);
+    $second_column = new Copy($copyAttributes, [new PreprocessedHTML([], get_field('field__copy__content_2'))]);
+
+    $component = new Columns(
+        $wrapperAttributes,
+        array(
+            new Column(
+                [],
+                [$first_column]
+            ),
+            new Column(
+                [],
+                [$second_column]
+            )
+        ));
+}
+else {
+    $component = new Copy(
+        [...$wrapperAttributes, 'colorTheme' => $block['colorTheme'] ?? $block['attributes']['colorTheme']['default']],
+        [new PreprocessedHTML([], get_field('copy'))]
+    );
+}
 
 $component->render();
