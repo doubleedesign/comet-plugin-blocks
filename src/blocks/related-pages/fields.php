ab9d70f8ba8f759a0f41aaf266241db584fcd9da
Leesa Ward <leesa@doubleedesign.com.au>
2026-01-01 20:35:21 +1100

    Feat(WP Blocks): Related Pages block

---
+++ src/blocks/related-pages/fields.php
@@ -0,0 +1,35 @@
+<?php
+acf_add_local_field_group(array(
+    'key'        => 'layout_related-pages',
+    'name'       => 'related_pages',
+    'label'      => 'Related pages',
+    'display'    => 'block',
+    'fields'     => array(
+        array(
+            'key'           => 'field__related-pages__heading',
+            'label'         => 'Heading',
+            'name'          => 'heading',
+            'type'          => 'text',
+            'default_value' => 'Related pages',
+            'placeholder'   => 'Related pages',
+        ),
+        array(
+            'key'           => 'field__related-pages__pages',
+            'label'         => 'Pages',
+            'name'          => 'pages',
+            'type'          => 'post_object',
+            'post_type'     => array('page'),
+            'multiple'      => true,
+            'return_format' => 'id',
+        ),
+    ),
+    'location' => array(
+        array(
+            array(
+                'param'    => 'block',
+                'operator' => '==',
+                'value'    => 'comet/related-pages',
+            ),
+        ),
+    )
+));
