b013ca18c573acaec58a25a04370b05c2eeee37e
Leesa Ward <leesa@doubleedesign.com.au>
2026-01-03 18:49:40 +1100

    Refactor(PreprocessedHTML): Deprecate WP class, move it into core

--- src/PreprocessedHTML.php
+++ src/PreprocessedHTML.php
@@ -3,16 +3,18 @@ namespace Doubleedesign\Comet\WordPress;
 
 /**
  * A class to handle the rendering of preprocessed HTML content
- * in a similar way to how Comet Components are rendered, for compatibility of handling
+ * in a similar way to how Comet Components are rendered, for compatibility of handling.
+ *
+ * @deprecated - use Comet Core PreprocessedHTML component instead
  */
 class PreprocessedHTML {
-	private string $content;
+    private string $content;
 
-	function __construct(array $attributes, string $content) {
-		$this->content = $content;
-	}
+    public function __construct(array $attributes, string $content) {
+        $this->content = $content;
+    }
 
-	public function render(): void {
-		echo $this->content;
-	}
+    public function render(): void {
+        echo $this->content;
+    }
 }
