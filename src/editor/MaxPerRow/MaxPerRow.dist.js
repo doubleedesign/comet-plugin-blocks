7b633a1be12124092d752fc1f9f6b9755d41dfc2
Leesa Ward <leesa@doubleedesign.com.au>
2026-01-07 11:02:01 +1100

    Feat(WP Blocks): Create itemCount and maxPerRow attribute controls
    To be used for upcoming events, and can be used for similar future uses like latest posts

---
+++ src/editor/MaxPerRow/MaxPerRow.dist.js
@@ -0,0 +1,30 @@
+import { FieldTooltip } from '../FieldTooltip/FieldTooltip.dist.js';
+
+/* eslint-disable max-len */
+const {
+  __experimentalNumberControl
+} = wp.components;
+const MaxPerRow = ({
+  attributes,
+  setAttributes
+}) => {
+  if (!attributes?.maxPerRow) {
+    return null;
+  }
+  const NumberControl = __experimentalNumberControl;
+  return /*#__PURE__*/React.createElement(NumberControl, {
+    __next40pxDefaultSize: true,
+    label: /*#__PURE__*/React.createElement(React.Fragment, null, "Max items per row", /*#__PURE__*/React.createElement(FieldTooltip, {
+      tooltip: 'The preferred number of items per row in containers wide enough to accommodate them; items may be stacked to a smaller number on smaller viewports'
+    })),
+    value: attributes.maxPerRow,
+    min: 2,
+    max: 4,
+    onChange: newMax => setAttributes({
+      maxPerRow: newMax
+    })
+  });
+};
+
+export { MaxPerRow };
+//# sourceMappingURL=MaxPerRow.dist.js.map
