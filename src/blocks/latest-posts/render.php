aac049837583379d67a89db268e8dc895017de76
Leesa Ward <leesa@doubleedesign.com.au>
2026-01-15 22:10:49 +1100

    Feat(WP Blocks LatestPosts): Create block

---
+++ src/blocks/latest-posts/render.php
@@ -0,0 +1,27 @@
+<?php
+/** @var $block array */
+
+use Doubleedesign\Comet\WordPress\TemplateUtils;
+
+$query = new WP_Query([
+    'post_type'      => 'post',
+    'posts_per_page' => get_field('number_of_posts') ?: 3,
+    'orderby'        => 'date',
+    'order'          => 'DESC',
+    'post_status'    => 'publish',
+    'tax_query'      => get_field('categories') ? [
+        [
+            'taxonomy' => 'category',
+            'field'    => 'term_id',
+            'terms'    => get_field('categories'),
+        ],
+    ] : [],
+]);
+$post_ids = wp_list_pluck($query->posts, 'ID');
+
+if (!$post_ids) {
+    return; // Do not render at all if the query returns no posts
+}
+
+$component = TemplateUtils::create_card_list($post_ids, $block);
+$component->render();
