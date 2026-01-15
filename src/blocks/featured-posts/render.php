27d02ac14773a5fd429e11618bfab5698cdad261
Leesa Ward <leesa@doubleedesign.com.au>
2026-01-15 22:08:08 +1100

    Feat(WP Blocks FeaturedPosts): Create block

---
+++ src/blocks/featured-posts/render.php
@@ -0,0 +1,13 @@
+<?php
+/** @var $block array */
+
+use Doubleedesign\Comet\WordPress\TemplateUtils;
+
+$page_ids = get_field('posts');
+
+if (!$page_ids) {
+    return; // Do not render at all if no posts are selected
+}
+
+$component = TemplateUtils::create_card_list($page_ids, $block);
+$component->render();
