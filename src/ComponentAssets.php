f2a24bf9d96435351a9c1abf9bd2b512ecf4adff
Leesa Ward <leesa@doubleedesign.com.au>
2026-01-15 22:04:10 +1100

    Feat(WP Blocks): Add class for adding global settings via plugin and use it to manage Font Awesome

--- src/ComponentAssets.php
+++ src/ComponentAssets.php
@@ -11,6 +11,7 @@ class ComponentAssets {
 
         add_action('wp_enqueue_scripts', [$this, 'enqueue_comet_combined_component_css'], 10);
         add_action('wp_enqueue_scripts', [$this, 'enqueue_comet_combined_component_js'], 10);
+        add_action('wp_enqueue_scripts', [$this, 'enqueue_font_awesome'], 10);
         add_filter('script_loader_tag', [$this, 'script_type_module'], 10, 3);
         add_filter('script_loader_tag', [$this, 'script_base_path'], 10, 5);
         add_action('admin_enqueue_scripts', [$this, 'enqueue_admin_css'], 10);
@@ -34,6 +35,18 @@ class ComponentAssets {
     public function enqueue_comet_combined_component_js(): void {
         $libraryDir = COMET_COMPOSER_VENDOR_URL . '/doubleedesign/comet-components-core';
         wp_enqueue_script('comet-components-js', "$libraryDir/dist/dist.js", array(), COMET_VERSION, true);
+    }
+
+    /**
+     * Font Awesome JS
+     *
+     * @return void
+     */
+    public function enqueue_font_awesome(): void {
+        $kit_id = get_option('options_font_awesome_kit');
+        if ($kit_id) {
+            wp_enqueue_script('font-awesome', "https://kit.fontawesome.com/$kit_id.js", [], '7', false);
+        }
     }
 
     /**
---
+++ src/GlobalSettings.php
@@ -0,0 +1,48 @@
+<?php
+namespace Doubleedesign\Comet\WordPress;
+
+class GlobalSettings {
+    public function __construct() {
+        add_filter('doublee_global_settings_fields', [$this, 'add_global_settings_fields'], 10, 1);
+        add_filter('doublee_global_settings_contributors', [$this, 'add_this_plugin_to_global_settings_about_tab'], 10, 1);
+
+    }
+
+    /**
+     * Add fields to the Global Options provided by the Double-E Design Base Plugin
+     * or registered by this one if that one is not active / has not registered the field group.
+     *
+     * @param  $fields
+     *
+     * @return array
+     */
+    public function add_global_settings_fields($fields): array {
+        $font_awesome_kit_field = array(
+            'key'               => 'field_font_awesome_kit',
+            'label'             => 'Font Awesome kit code (for icons)',
+            'name'              => 'font_awesome_kit',
+            'type'              => 'text',
+            'instructions'      => 'If your developer has not included this, you can sign up and create a kit at https://fontawesome.com/',
+        );
+
+        // Find the "Accounts & Assets" tab
+        $index = array_search('Accounts & Assets', array_column($fields['fields'], 'label'));
+        if ($index !== false) {
+            // Insert the new field after the tab
+            array_splice($fields['fields'], $index + 1, 0, [$font_awesome_kit_field]);
+        }
+        else {
+            // If the tab is not found, just append the field to the end
+            $fields['fields'][] = $font_awesome_kit_field;
+        }
+
+        return $fields;
+    }
+
+    public function add_this_plugin_to_global_settings_about_tab($contributors) {
+        array_push($contributors, 'Comet Components plugin');
+
+        return $contributors;
+    }
+
+}
