8ecac00699046e8131c91bec971f00766e1efd13
Leesa Ward <leesa@doubleedesign.com.au>
2026-02-02 10:41:15 +1100

    Feat(Gallery): Make into a top-level section component; improve lightbox handling; create WP Block for it

--- src/blocks/gallery/render.php
+++ src/blocks/gallery/render.php
@@ -1,3 +1,30 @@
 <?php
-echo 'TODO: Implement block rendering logic';
-echo 'TODO: Implement aspect ratio field and lightbox toggle in the editor sidebar';
+/** @var $block array */
+
+use Doubleedesign\Comet\Core\{AspectRatio, ContentImageBasic, Gallery, Utils};
+use Doubleedesign\Comet\WordPress\BlockRenderer;
+
+$is_editor = isset($is_preview) && $is_preview;
+$render_placeholder = BlockRenderer::maybe_render_editor_placeholder($block, $is_editor);
+if ($render_placeholder) {
+    return;
+}
+
+$images = get_field('images');
+$caption = get_field('caption');
+$attributes = Utils::array_pick($block, ['tagName', 'size', 'maxPerRow', 'lightbox']);
+
+$images = array_map(function($image) use ($block) {
+    $attrs = $block['captions'] ? Utils::array_pick($image, ['alt', 'caption']) : Utils::array_pick($image, ['alt']);
+    $attrs['src'] = $image['sizes']['large']; // The image to display as the thumbnail in the gallery
+    $attrs['aspectRatio'] = $block['aspectRatio'] ?? AspectRatio::SQUARE->value;
+
+    if (isset($block['lightbox']) && $block['lightbox']) {
+        $attrs['href'] = $image['url'];
+    }
+
+    return new ContentImageBasic($attrs);
+}, $images ?? []);
+
+$component = new Gallery([...$attributes, 'imageCrop' => true, 'caption' => $caption], $images);
+$component->render();
