ef969b1d65ff81ea255d2df4e8fe2213892cd6d9
Leesa Ward <leesa@doubleedesign.com.au>
2026-02-01 15:20:51 +1100

    Fix(WP Blocks Copy): Remove 2-column option
    It's buggy and not in use yet anyway

--- src/blocks/copy/fields.php
+++ src/blocks/copy/fields.php
@@ -14,38 +14,6 @@ acf_add_local_field_group(array(
             'delay'             => false,
             'repeatable'        => true,
         ),
-        array(
-            'key'           => 'field__copy__content__split',
-            'label'         => 'Split into two columns',
-            'name'          => 'two_column',
-            'instructions'  => 'Split into two content columns. Note: The content will appear stacked in one column on small viewports.',
-            'type'          => 'true_false',
-            'ui'            => 1,
-            'ui_on_text'    => 'Yes',
-            'ui_off_text'   => 'No',
-            'default_value' => false,
-            'repeatable'    => false
-        ),
-        array(
-            'key'               => 'field__copy__content_2',
-            'label'             => 'Content (second column)',
-            'name'              => 'copy_2',
-            'type'              => 'wysiwyg',
-            'tabs'              => 'all',
-            'toolbar'           => 'full',
-            'media_upload'      => true,
-            'delay'             => false,
-            'repeatable'        => true,
-            'conditional_logic' => array(
-                array(
-                    array(
-                        'field'    => 'field__copy__content__split',
-                        'operator' => '==',
-                        'value'    => '1'
-                    ),
-                ),
-            ),
-        ),
     ),
     'location' => array(
         array(
--- src/blocks/copy/render.php
+++ src/blocks/copy/render.php
@@ -1,7 +1,7 @@
 <?php
 /** @var $block array */
 
-use Doubleedesign\Comet\Core\{Column, Columns, Copy, PreprocessedHTML};
+use Doubleedesign\Comet\Core\{Copy, PreprocessedHTML};
 use Doubleedesign\Comet\WordPress\{BlockRenderer};
 
 $is_editor = isset($is_preview) && $is_preview;
@@ -10,38 +10,14 @@ if ($render_placeholder) {
     return;
 }
 
-$has_second_column = get_field('field__copy__content__split') && !empty(get_field('field__copy__content_2'));
 $wrapperAttributes = [
     'size'       => $block['size'] ?? $block['attributes']['size']['default'],
+    'tagName'    => $block['tagName'] ?? 'section'
 ];
 
-if ($has_second_column) {
-    $copyAttributes = [
-        'colorTheme' => $block['colorTheme'] ?? $block['attributes']['colorTheme']['default'],
-        'isNested'   => true,
-    ];
-
-    $first_column = new Copy($copyAttributes, [new PreprocessedHTML([], get_field('copy'))]);
-    $second_column = new Copy($copyAttributes, [new PreprocessedHTML([], get_field('field__copy__content_2'))]);
-
-    $component = new Columns(
-        $wrapperAttributes,
-        array(
-            new Column(
-                [],
-                [$first_column]
-            ),
-            new Column(
-                [],
-                [$second_column]
-            )
-        ));
-}
-else {
-    $component = new Copy(
-        [...$wrapperAttributes, 'colorTheme' => $block['colorTheme'] ?? $block['attributes']['colorTheme']['default']],
-        [new PreprocessedHTML([], get_field('copy'))]
-    );
-}
+$component = new Copy(
+    [...$wrapperAttributes, 'colorTheme' => $block['colorTheme'] ?? $block['attributes']['colorTheme']['default']],
+    [new PreprocessedHTML([], get_field('copy'))]
+);
 
 $component->render();
