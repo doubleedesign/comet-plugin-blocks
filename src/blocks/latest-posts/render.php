aa439e0035a30dc9daac71130159dc73de0ec7a7
Leesa Ward <leesa@doubleedesign.com.au>
2026-02-02 10:43:50 +1100

    Link option

--- src/blocks/latest-posts/render.php
+++ src/blocks/latest-posts/render.php
@@ -5,7 +5,7 @@ use Doubleedesign\Comet\WordPress\TemplateUtils;
 
 $query = new WP_Query([
     'post_type'      => 'post',
-    'posts_per_page' => get_field('number_of_posts') ?: 3,
+    'posts_per_page' => get_field('item_count') ?: 3,
     'orderby'        => 'date',
     'order'          => 'DESC',
     'post_status'    => 'publish',
@@ -23,5 +23,16 @@ if (!$post_ids) {
     return; // Do not render at all if the query returns no posts
 }
 
-$component = TemplateUtils::create_card_list($post_ids, $block);
+$link = apply_filters('comet_blocks_latest_posts_link', [
+    'url'       => get_permalink(get_option('page_for_posts')),
+    'title'     => 'View all articles',
+    'isOutline' => true
+], $post_ids);
+
+$component = TemplateUtils::create_card_list(
+    $post_ids,
+    $block,
+    true,
+    !empty($link) ? $link : null
+);
 $component->render();
