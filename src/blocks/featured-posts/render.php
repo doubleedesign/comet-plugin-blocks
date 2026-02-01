ee6403d13ad3bb1c86dafbbb4dbdc48d3db8604c
Leesa Ward <leesa@doubleedesign.com.au>
2026-02-02 10:43:50 +1100

    Link option

--- src/blocks/featured-posts/render.php
+++ src/blocks/featured-posts/render.php
@@ -9,5 +9,5 @@ if (!$page_ids) {
     return; // Do not render at all if no posts are selected
 }
 
-$component = TemplateUtils::create_card_list($page_ids, $block);
+$component = TemplateUtils::create_card_list($page_ids, $block, true);
 $component->render();
