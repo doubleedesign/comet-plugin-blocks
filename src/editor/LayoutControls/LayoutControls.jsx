fe1a714e156c90dd935213f06b56a24d9c9b5d2d
Leesa Ward <leesa@doubleedesign.com.au>
2026-02-02 10:53:15 +1100

    fixup! Feat(Gallery): Make into a top-level section component; improve lightbox handling; create WP Block for it

--- src/editor/LayoutControls/LayoutControls.jsx
+++ src/editor/LayoutControls/LayoutControls.jsx
@@ -10,6 +10,8 @@ import { MaxPerRow } from '../MaxPerRow/MaxPerRow';
 import { ItemCount } from '../ItemCount/ItemCount.jsx';
 import { NegativeMargins } from '../NegativeMargins/NegativeMargins.jsx';
 import { AspectRatio } from '../AspectRatio/AspectRatio.jsx';
+import { AspectRatio } from '../AspectRatio/AspectRatio.jsx';
+import { GalleryControls } from '../GalleryControls/GalleryControls.jsx';
 import { GalleryControls } from '../GalleryControls/GalleryControls.jsx';
 
 export const LayoutControls = (props) => {
