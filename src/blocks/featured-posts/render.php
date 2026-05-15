1b96afc526477c174404663d233645d319f4a017
Leesa Ward <leesa@doubleedesign.com.au>
2026-05-15 17:21:25 +1000

    Assorted styling tweaks

--- src/blocks/featured-posts/render.php
+++ src/blocks/featured-posts/render.php
@@ -1,13 +1,18 @@
 <?php
 /** @var $block array */
 
-use Doubleedesign\Comet\WordPress\TemplateUtils;
+use Doubleedesign\Comet\WordPress\{BlockRenderer, TemplateUtils};
 
-$page_ids = get_field('posts');
+$is_editor = isset($is_preview) && $is_preview;
+$render_placeholder = BlockRenderer::maybe_render_editor_placeholder($block, $is_editor);
+if ($render_placeholder) {
+	return;
+}
 
-if (!$page_ids) {
+$post_ids = function_exists('get_field') ? get_field('posts') : [];
+if (!$post_ids) {
     return; // Do not render at all if no posts are selected
 }
 
-$component = TemplateUtils::create_card_list($page_ids, $block, true);
+$component = TemplateUtils::create_card_list($post_ids, $block, true);
 $component->render();
